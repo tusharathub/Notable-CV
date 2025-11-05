e172924c7673238fbd8c1b4970a1fa2990cfe720
Tushar <tushar.nyc20@gmail.com>
2025-11-05 15:42:38 +0530

    wrapped layout with convexClientProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import ConvexClientProvider from "./ConvexClerkProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,14 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <ConvexClientProvider>
           {children}
+          </ConvexClientProvider>
         </body>
       </html>
-    </ClerkProvider>
   );
 }
