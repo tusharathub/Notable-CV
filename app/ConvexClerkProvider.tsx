c8829904a88181f4ef5e217b4e0ee882b8d90bb8
Tushar <tushar.nyc20@gmail.com>
2025-11-05 15:40:44 +0530

    added convexClerkProvider

---
+++ app/ConvexClerkProvider.tsx
@@ -0,0 +1,15 @@
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ReactNode } from "react";
+
+
+if(!process.env.NEXT_PUBLIC_CONVEX_URL) {
+    throw new Error("Missing NEXT_PUBLIC_CONVEX_URL in you .env file");
+}
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+
+export default function ConvexClientProvider({children} : {children: ReactNode}) {
+    return (
+        <ConvexProvider client={convex}> {children} </ConvexProvider>
+    )
+}
