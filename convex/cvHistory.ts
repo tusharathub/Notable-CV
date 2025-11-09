fa4319aae85fde64cf9d0db295034f289a9201a6
Tushar <tushar.nyc20@gmail.com>
2025-11-09 13:58:12 +0530

    cvHistory queries update

--- convex/cvHistory.ts
+++ convex/cvHistory.ts
@@ -2,7 +2,7 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 
-export const saveCv = mutation({
+export const saveCvHistory = mutation({
     args: {userId: v.string(), title: v.string(), content: v.string()},
     handler: async (ctx, {userId, title, content}) => {
         const user = await ctx.db
@@ -11,35 +11,34 @@ export const saveCv = mutation({
         .unique();
 
         if(!user) throw new Error("user not found");
+        if(!user.isPremium) return;
 
-        const existingCVs = await ctx.db
-        .query("cvHistory")
-        .withIndex("by_userId", (q) => q.eq("userId", userId))
-        .collect();
-
-        if(!user.isPremium && existingCVs.length >= 3) {
-            throw new Error("Freee user only can save upto 3 CVs");
-        }
-
-        const CvId = await ctx.db.insert("cvHistory", {
+        await ctx.db.insert("cvHistory", {
             userId,
             title,
             content,
             createdAt: Date.now(),
         });
 
-        return CvId;
     }
 })
 
-export const getUserHistory = query({
+export const getCVHistory = query({
     args: {userId: v.string()},
     handler: async (ctx, {userId}) => {
+
+        const user = await ctx.db
+        .query("users")
+        .withIndex("by_userId", (q) => q.eq("userId", userId))
+        .unique();
+
+        if(!user || !user.isPremium) return [];
+
         const history = await ctx.db
         .query("cvHistory")
         .withIndex("by_userId", (q) => q.eq("userId", userId))
         .order("desc")
-        .collect();
+        .take(20);
 
         return history;
     }
